90a61dac0ac055a82e980764e334ed521c4db890
Sandervinipux <57655774+Sandervinipux@users.noreply.github.com>
2021-09-01 13:31:27 +0300

    Add files via upload
    function

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 "use strict";
+let number = 10;
+//Замыкания (сама функция, вместе с тем что ей доступно)
+function showMessage(text) {
+    console.log(text);
+    //let number = 20;
+    console.log(number);
 
-const typeGoods = 'food'
+}
+
+showMessage("Hello!!!");
+console.log(number);
+
+const server = function(){
+    console.log('server start...');
+
+};
+
+server();
+
+const calc = (x, y) => x+y;
+//const calc = (x, y) => {
+//  return x+y    
+//};
+
+console.log(calc(10,5));
+
+//Задание приветствие и число п
+
+"use strict";
+let number = 3.14;
+function showMessage(text) {
+    console.log(text);
+    console.log(number);
+
+}
+
+showMessage("Hello!!!");
+console.log(number);
+
+
+server();
